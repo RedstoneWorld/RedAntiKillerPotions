024f0075156ddbfed8f9a34564aa6154434a1371
RedstoneFuture <redstonefuture@redstoneworld.de>
2023-10-05 16:37:12 +0200

    Refactoring

--- src/main/java/de/redstoneworld/redantikillerpotions/RedAntiKillerPotions.java
+++ src/main/java/de/redstoneworld/redantikillerpotions/RedAntiKillerPotions.java
@@ -10,7 +10,6 @@
 
 package de.redstoneworld.redantikillerpotions;
 
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RedAntiKillerPotions extends JavaPlugin {
@@ -28,22 +27,5 @@ public class RedAntiKillerPotions extends JavaPlugin {
 	public void onDisable() {
 		
 	}
-
-	/**
-	 * This method reads the specific messages in config.yml and replaces
-	 * the minecraft color codes with a valid character.
-	 * 
-	 * @param key YAML key
-	 * @param args placeholder without "%" and value for the placeholder
-	 * 
-	 * @return the config messages (String)
-	 */
-	String getLang(String key, String... args) {
-		String lang = getConfig().getString("messages." + key, "&cUnknown language key &6" + key);
-		for (int i = 0; i + 1 < args.length; i += 2) {
-			lang = lang.replace("%" + args[i] + "%", args[i + 1]);
-		}
-		return ChatColor.translateAlternateColorCodes('&', lang);
-	}
-
+	
 }
